c98ee8e3e9baeae7dd5e3dff881eb5118b514725
Elvis020 <elvis.amoako@stu.ucc.edu.gh>
2020-12-27 00:42:05 +0000

    😋Added tweaks and Services section😋

--- arcdev/src/components/LandingPage.js
+++ arcdev/src/components/LandingPage.js
@@ -4,6 +4,8 @@ import { makeStyles, useTheme } from "@material-ui/styles";
 import animationData from "../animations/landingAnimation/data";
 import { Grid, Button, Typography } from "@material-ui/core";
 import { ButtonArrow } from "../components/ui";
+import customSoftwareIcon from "../assets/Custom_Software_Icon.svg";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 
 // Setting up useStyles
 const useStyles = makeStyles((theme) => ({
@@ -12,9 +14,9 @@ const useStyles = makeStyles((theme) => ({
     minWidth: "21em",
     marginTop: "2em",
     marginLeft: "10%",
-    [theme.breakpoints.down("sm")]:{
+    [theme.breakpoints.down("sm")]: {
       maxWidth: "30em",
-  },
+    },
   },
   estimateButton: {
     ...theme.typography.estimate,
@@ -32,32 +34,61 @@ const useStyles = makeStyles((theme) => ({
     marginTop: "1em",
   },
   learnButtonHero: {
-    borderColor: theme.palette.common.blue,
-    color: theme.palette.common.blue,
-    borderWidth: 2,
-    textTransform: "none",
-    borderRadius: 50,
-    fontFamily: "Roboto",
-    fontWeight: "bold",
+    ...theme.typography.learnButton,
     fontSize: "0.9rem",
     height: 45,
     width: 145,
   },
   mainContainer: {
     marginTop: "3.5em",
-    [theme.breakpoints.down("md")]:{
-        marginTop:"3em",
+    [theme.breakpoints.down("md")]: {
+      marginTop: "3em",
     },
-    [theme.breakpoints.down("xs")]:{
-        marginTop:"2em",
+    [theme.breakpoints.down("xs")]: {
+      marginTop: "2em",
     },
   },
-  
+  heroTextContainer: {
+    minWidth: "21.5em",
+    marginLeft: "1em",
+    [theme.breakpoints.down("xs")]: {
+      marginLeft: 0,
+    },
+  },
+  specialText: {
+    fontFamily: "Pacifico",
+    color: theme.palette.common.orange,
+  },
+  learnButton: {
+    ...theme.typography.learnButton,
+    height: 35,
+    fontSize: "0.7rem",
+    [theme.breakpoints.down("sm")]: {
+      marginBottom: "2em",
+    },
+  },
+  subtitle: {
+    marginBottom: ".4em",
+  },
+  icon: {
+    marginLeft: "2em",
+    [theme.breakpoints.down("sm")]: {
+      marginLeft: 0,
+    },
+  },
+  serviceContainer: {
+    marginTop: "12em",
+    [theme.breakpoints.down("sm")]: {
+      padding: 25,
+    },
+  },
 }));
 
 const LandingPage = () => {
   const elvisUI = useStyles();
   const theme = useTheme();
+  // For media queries
+  const matchesSM = useMediaQuery(theme.breakpoints.down("sm"));
 
   // For react-lottie
   const defaultOptions = {
@@ -72,6 +103,8 @@ const LandingPage = () => {
   return (
     <Grid container className={elvisUI.mainContainer} direction="column">
       <Grid item>
+        {" "}
+        {/* ------ Hero Block ------- */}
         <Grid container justify="flex-end" alignItems="center" direction="row">
           {/* Text */}
           <Grid className={elvisUI.heroTextContainer} sm item>
@@ -104,6 +137,50 @@ const LandingPage = () => {
           {/* Animation */}
           <Grid className={elvisUI.animation} sm item>
             <Lottie options={defaultOptions} height={"100%"} width={"100%"} />
+          </Grid>
+        </Grid>
+      </Grid>
+
+      {/* ------ Services Block ------- */}
+      <Grid item style={{ margin: "0 1em" }}>
+        <Grid
+          container
+          justify={matchesSM && "center"}
+          direction="row"
+          className={elvisUI.serviceContainer}
+        >
+          {/* First Item */}
+          <Grid
+            item
+            style={{
+              marginLeft: matchesSM ? 0 : "5em",
+              textAlign: matchesSM && "center",
+            }}
+          >
+            <Typography variant="h4">Custom Software Development</Typography>
+            <Typography variant="subtitle1" className={elvisUI.subtitle}>
+              Save Energy, Save Time, Save Money.
+            </Typography>
+            <Typography style={{ marginBottom: "1em" }} variant="subtitle1">
+              Complete digital solutions, from investigation to{" "}
+              <span className={elvisUI.specialText}>celebration.</span>
+            </Typography>
+            <Button variant="outlined" className={elvisUI.learnButton}>
+              <span style={{ marginRight: 6 }}> Learn More </span>
+              <ButtonArrow
+                width={10}
+                height={10}
+                fill={theme.palette.common.blue}
+              />
+            </Button>
+          </Grid>
+          {/* First Icon */}
+          <Grid item>
+            <img
+              className={elvisUI.icon}
+              src={customSoftwareIcon}
+              alt="Custom software Icon"
+            />
           </Grid>
         </Grid>
       </Grid>
--- arcdev/src/components/ui/Theme.js
+++ arcdev/src/components/ui/Theme.js
@@ -3,18 +3,19 @@ import { createMuiTheme } from "@material-ui/core/styles";
 // Creating constants for main colors
 const arcBlue = "#0b72b9";
 const arcOrange = "#ffba60";
+const arcGrey = "#868686";
 
 export default createMuiTheme({
   palette: {
     common: {
-      blue: `${arcBlue}`,
-      orange: `${arcOrange}`,
+      blue: arcBlue,
+      orange: arcOrange,
     },
     primary: {
-      main: `${arcBlue}`,
+      main: arcBlue,
     },
     secondary: {
-      main: `${arcOrange}`,
+      main: arcOrange,
     },
   },
   typography: {
@@ -34,18 +35,33 @@ export default createMuiTheme({
       fontFamily: "Raleway",
       fontWeight: 600,
       fontSize: "2.5rem",
-      color: `${arcBlue}`,
+      color: arcBlue,
       lineHeight: 1.3,
     },
     h3: {
       fontFamily: "Pacifico",
       fontSize: "2.5rem",
-      color: `${arcBlue}`,
+      color: arcBlue,
     },
     h4: {
-      fontFamily: "Pacifico",
+      fontFamily: "Raleway",
       fontSize: "1.75rem",
-      color: `${arcBlue}`,
+      color: arcBlue,
+      fontWeight: 600,
+    },
+    subtitle1: {
+      fontSize: "1.25rem",
+      fontWeight: 300,
+      color: arcGrey,
+    },
+    learnButton: {
+      borderColor: arcBlue,
+      color: arcBlue,
+      borderWidth: 2,
+      textTransform: "none",
+      borderRadius: 50,
+      fontFamily: "Roboto",
+      fontWeight: "bold",
     },
   },
 });
